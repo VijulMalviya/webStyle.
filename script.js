5c76145024c26de5367c4cc715bf1f525e4fcc04
Vijul Malviya <50556784+VijulMalviya@users.noreply.github.com>
2021-01-19 13:24:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+$('.nav-circle').click(() =>{
+
+    $('.layer').css("left","0%");
+});
+$('.cross').click(() =>{
+    $('.layer').css("left","-100%");
+});
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    $("nav").css("background-color","white")
+  } else {
+    $("nav").css("background-color","transparent")
+  }
+}
